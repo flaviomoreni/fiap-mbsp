85be1070fa00898522debed1622f40271bdd2c98
Pedro Ivo Correia <pedroivocorreia@Pedros-MacBook-Pro.local>
2019-07-18 01:16:35 -0300

    criacao do projeto

---
+++ src/main/java/br/com/fiap/mpsp/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package br.com.fiap.mpsp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("fiap").password(encoder.encode("mpsp")).roles("USER");
+	}
+	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/arisp/**").and().ignoring().antMatchers("/arpensp/**");
+    }
+	
+}
